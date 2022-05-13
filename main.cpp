483f6e753fe3602e98f609935b26a64d6ab41cd1
s24965-pj <92219386+Dawid0132@users.noreply.github.com>
2022-05-13 20:02:05 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,23 +1,42 @@
 #include <iostream>
-
-int main() {
-    int wysokość=0;
-    std::cout<<"Wprowadź wysokość: ";
-    std::cin>>wysokość;
-    for (int i = 0; i < wysokość; ++i) {
-        const int miejsca=wysokość - i;
-        for (int j = 0; j < miejsca; ++j) {
-            std::cout<<" ";
+#include <vector>
+#include <cstdio>
+int main(int argc, char *argv[]) {
+    FILE *pFfile;
+    pFfile = fopen(argv[1],"r");
+    if (pFfile) {
+        int x;
+        int number_of_lines = 0;
+        while ((x = getc(pFfile)) != EOF) {
+            if ('\n' == x) {
+                number_of_lines++;
+            }
         }
-        const int gwiazdki=i*2+1;
-        for (int j = 0; j < gwiazdki; ++j) {
-            std::cout<<"*";
+            fclose(pFfile);
+            int *my_ptr;
+            my_ptr= (int *)(malloc(sizeof(int) * number_of_lines));
+            pFfile = fopen("liczby.txt","r");
+            for (int i = 0; i < number_of_lines+1; ++i) {
+                fscanf(pFfile, "%i", &my_ptr[i]);
+            }
+            for (int i=number_of_lines; i>0; i--) {
+                std::cout<<my_ptr[i];
+            }
+        free(my_ptr);
+            fclose(pFfile);
+        } else {
+        int x,y;
+        std::cout<<"Ile liczb chcesz wprowadzić?\n";
+        std::cin>>x;
+        std::vector<int> liczby;
+        for (int i = 0; i < x; ++i) {
+            std::cout<<"Wprowadź liczbę: ";
+            std::cin>>y;
+            liczby.push_back(y);
         }
-        std::cout<<'\n';
+        for (int i = liczby.size()-1; i >= 0; i--) {
+            std::cout<<liczby[i];
+        }
     }
-    for (int i = 0; i < wysokość; ++i) {
-        std::cout<<" ";
-    }
-    std::cout<<"*";
     return 0;
 }
