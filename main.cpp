ab25e547350f2d8b19134bdf70a87417c5ff88b6
s24965-pj <92219386+Dawid0132@users.noreply.github.com>
2022-05-13 19:55:30 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,24 +1,18 @@
 #include <iostream>
 #include <vector>
- int suma(std::vector<int> liczby,int x , int y){
-    int suma=0;
-    for (int i = 0; i < x; ++i) {
+int main() {
+    int a,b,suma=0;
+    std::cout<<"Ile liczb chcesz wprowadzić?"<<'\n';
+    std::cin>>b;
+    std::vector<int> liczby;
+    for (int i = 0; i < b; i++){
         std::cout<<"Wprowadź liczbę: ";
-        std::cin>>y;
-        liczby.push_back(y);
+        std::cin>>a;
+        liczby.push_back(a);
     }
     for (int i = 0; i < liczby.size(); ++i) {
-        suma += liczby[i];
+        suma+=liczby[i];
     }
-    std::cout<<"Suma wynosi: ";
-     return suma;
-}
-
-int main() {
-    int x,y=0;
-    std::cout<<"Wprowadź ilość liczb: ";
-    std::cin>>x;
-    std::vector<int> liczby;
-    std::cout<<suma(liczby,x,y);
+    std::cout<<"Suma wynosi: "<<suma<<'\n';
     return 0;
 }
